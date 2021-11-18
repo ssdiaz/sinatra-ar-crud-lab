93c7cd5cd16744df27d4391873106f626eca751d
Samantha Diaz <samidiaz3@gmail.com>
2021-11-17 23:25:55 -0500

    commit tree d8e73ab72ee7f405df15b10be4e0f79ed61fc1be

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,29 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/new' do 
+    erb :new
+  end
+
+  post '/articles' do
+   @article = Article.new(params)
+   redirect to "/articles/#{@article.ed}"
+  end
+
+  get '/articles/:id/edit' do 
+    erb :edit
+  end
+
 end
+
